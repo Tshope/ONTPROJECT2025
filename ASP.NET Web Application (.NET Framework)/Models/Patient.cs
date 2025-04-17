fd2ba2dc5c934a63c21b33073d8b2fc1a10d51a8
90Oazir <95278206+WisemanSilinda@users.noreply.github.com>
2025-04-17 16:57:47 +0200

    Patient class added

---
+++ ASP.NET Web Application (.NET Framework)/Models/Patient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Web_Application__.NET_Framework_.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+
+        // Notification preferences
+        public bool EmailNotificationEnabled { get; set; }
+        public bool SmsNotificationEnabled { get; set; }
+        public bool PushNotificationEnabled { get; set; }
+
+        // Timestamps
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Patient()
+        {
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+            EmailNotificationEnabled = false;
+            SmsNotificationEnabled = false;
+            PushNotificationEnabled = false;
+        }
+    }
+}
