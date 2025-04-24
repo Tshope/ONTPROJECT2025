4fe606ba229d87285ba636edc2bfe8b6796f5776
90Oazir <95278206+WisemanSilinda@users.noreply.github.com>
2025-04-24 10:30:37 +0200

    observer classes adjusted to fit the patientid

---
+++ ASP.NET Web Application (.NET Framework)/Models/StrategyPatternModel/ConcreteStrategyBaseScheduledDelivery.cs
@@ -0,0 +1,40 @@
+﻿using ASP.NET_Web_Application__.NET_Framework_.Data;
+using ASP.NET_Web_Application__.NET_Framework_.Models.StrategyPatternModel.ASP.NET_Web_Application__.NET_Framework_.Models;
+using ASP.NET_Web_Application__.NET_Framework_.Models.StrategyPatternModel.ASP.NET_Web_Application__.NET_Framework_.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace ASP.NET_Web_Application__.NET_Framework_.Models.StrategyPatternModel
+{
+	public class ConcreteStrategyBaseScheduledDelivery
+	{
+        public class ScheduledDeliveryStrategy : INotificationDeliveryStrategy
+        {
+            public string StrategyName => "Scheduled Delivery";
+            private readonly NotificationDAL _dal = NotificationDAL.GetInstance();
+            private readonly DateTime _deliveryTime;
+
+            public ScheduledDeliveryStrategy(DateTime deliveryTime)
+            {
+                _deliveryTime = deliveryTime;
+            }
+
+            public void Deliver(List<Notification> notifications)
+            {
+                var timeUntilDelivery = _deliveryTime - DateTime.Now;
+                if (timeUntilDelivery > TimeSpan.Zero)
+                {
+                    Thread.Sleep(timeUntilDelivery);
+                }
+
+                foreach (var notification in notifications)
+                {
+                    _dal.UpdateNotificationStatus(notification.NotificationId, "Delivered", StrategyName);
+                }
+            }
+        }
+    }
+}
---
+++ ASP.NET Web Application (.NET Framework)/Models/StrategyPatternModel/StrategyBase.cs
@@ -0,0 +1,14 @@
+﻿using ASP.NET_Web_Application__.NET_Framework_.Models.StrategyPatternModel.ASP.NET_Web_Application__.NET_Framework_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Web_Application__.NET_Framework_.Models.StrategyPatternModel
+{
+    public abstract class StrategyBase
+    {
+        public abstract string StrategyName { get; }
+        public abstract void Deliver(List<Notification> notifications);
+    }
+}
