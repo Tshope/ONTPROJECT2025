e227bbe6f9c74bf52f53ba9532a181eef09cf17f
Aphelele Ayolisile Marwarwa <marwarwa.ap@gmail.com>
2025-05-05 23:36:49 +0200

    SMTP

--- ASP.NET Web Application (.NET Framework)/Models/Notifications/EmailNotification.cs
+++ ASP.NET Web Application (.NET Framework)/Models/Notifications/EmailNotification.cs
@@ -1,12 +1,39 @@
 ﻿using ASP.NET_Web_Application__.NET_Framework_.Models;
 using System;
+using System.Net.Mail;
+using System.Configuration;
+using System.Net;
 namespace ASP.NET_Web_Application__.NET_Framework_.Models.Notifications
 {
     public class EmailNotification : INotification
     {
         public string Send(string recipient, string message)
         {
-            return $"[EMAIL to {recipient}]: {message}";
+            try
+            {
+                var smtpUser = ConfigurationManager.AppSettings["SmtpUser"];
+                var smtpPassword = ConfigurationManager.AppSettings["SmtpPassword"];
+                var smtpServer = ConfigurationManager.AppSettings["SmtpServer"];
+                var smtpPort = int.Parse(ConfigurationManager.AppSettings["SmtpPort"]);
+
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(smtpUser);
+                mail.To.Add(recipient);
+                mail.Subject = "Healthcare Appointment Reminder";
+                mail.Body = message;
+
+                SmtpClient smtp = new SmtpClient(smtpServer, smtpPort);
+                smtp.Credentials = new NetworkCredential(smtpUser, smtpPassword);
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+
+                return $"Email sent to {recipient}";
+            }
+            catch (Exception ex)
+            {
+                return $"Failed to send email: {ex.Message}";
+            }
         }
     }
 }
