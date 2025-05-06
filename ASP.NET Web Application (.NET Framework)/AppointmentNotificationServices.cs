0f93eaccaec1521542d500e7f4451e18276ed729
90Oazir <95278206+WisemanSilinda@users.noreply.github.com>
2025-05-06 11:54:14 +0200

    pushed

--- ASP.NET Web Application (.NET Framework)/AppointmentNotificationServices.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ASP.NET_Web_Application__.NET_Framework_
-{
-    internal class AppointmentNotificationServices
-    {
-        public AppointmentNotificationServices()
-        {
-        }
-    }
-}
